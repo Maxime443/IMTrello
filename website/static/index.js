f22dafab53dd446ad4c240e2d4d3d494469fc064
XxWillyxX <william.bresso@gmail.com>
2024-02-15 18:02:07 +0100

    rf

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,18 @@
 function deleteNote(noteId) {
+  console.log("Delete button is clicked for note ID: " + noteId);
   fetch("/delete-note", {
     method: "POST",
     body: JSON.stringify({ noteId: noteId }),
   }).then((_res) => {
-    window.location.href = "/";
+    window.location.href = "/home2";
   });
 }
+
+function changeNote(button, noteId) {
+  // Add a CSS class to change the color of the button
+  button.classList.add("clicked");
+  console.log("Change button clicked for note ID: " + noteId);
+  // Here you can perform other actions, such as showing a new button
+  // or performing any other logic related to the note ID
+}
+
--- website/views.py
+++ website/views.py
@@ -23,8 +23,36 @@ def home():
             return redirect(url_for('views.home'))
     return render_template("home.html",user=current_user)
 
+@views.route('/home2', methods=['GET', 'POST'])
+@login_required
+def home2():
+    if request.method == 'POST':
+        text = request.form.get('note')
+        if len(text) < 1:
+            flash('Note is too short.', category='error')
+        else:
+            flash('Note added', category='success')
+            new_note = Note(data=text, user_id=current_user.id,date=datetime.now())
+            db.session.add(new_note)
+            db.session.commit()
+            return redirect(url_for('views.home2'))
+    return render_template("home2.html",user=current_user)
+
 @views.route('/delete-note', methods=['POST'])
+@login_required
 def delete_note():
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
+
+@views.route('/change-note', methods=['GET', 'POST'])
+@login_required
+def change_note():
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file
     noteId = note['noteId']
     note = Note.query.get(noteId)
