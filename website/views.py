90fb5dd7d0ee96cfe779cb47d82543ae1be433cb
XxWillyxX <william.bresso@gmail.com>
2024-02-17 13:01:07 +0100

    rf

--- website/views.py
+++ website/views.py
@@ -12,19 +12,53 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if request.method == 'POST':
-        name = request.form.get('name')
-        description = request.form.get('description')
-        date = request.form.get('end_date')
-        end_date=datetime.strptime(date, '%Y-%m-%d').date()
-        if len(name) < 1:
-            flash('Project is too short.', category='error')
+        # checking if this is the project creation form
+        if 'name' in request.form and 'description' in request.form and 'end_date' in request.form:
+            name = request.form.get('name')
+            description = request.form.get('description')
+            date = request.form.get('end_date')
+            end_date=datetime.strptime(date, '%Y-%m-%d').date()
+            if len(name) < 1:
+                flash('Project name is too short.', category='error')
+            else:
+                flash('Project added', category='success')
+                new_project = Project(name=name, admin_id=current_user.id, description=description, end_date=end_date, sections=[])
+                db.session.add(new_project)
+                current_user.projects.append(new_project)
+                db.session.commit()
+                return redirect(url_for('views.home'))
+        # otherwise it's the project renaming form
         else:
-            flash('Project added', category='success')
-            new_project = Project(name=name, admin_id=current_user.id, description=description, end_date=end_date, sections=[])
-            db.session.add(new_project)
-            current_user.projects.append(new_project)
-            db.session.commit()
-            return redirect(url_for('views.home'))
+            name = request.form.get('name1')
+            project_id = request.form.get('project_id')
+            if len(name) < 1:
+                flash('Project is too short.', category='error')
+            else:
+                flash('Project renamed', category='success')
+                old_project = Project.query.get(project_id)
+                new_project = Project()  # Create a new instance of the Project model
+                new_project.name = name  # Assign the new name to the new project
+                # Copy other attributes as needed
+                new_project.id = old_project.id
+                new_project.admin_id = old_project.admin_id
+                new_project.end_date = old_project.end_date
+                new_project.description = old_project.description
+                new_project.sections = []
+                for section in old_project.sections:
+                    new_project.sections.append(section)
+
+                if old_project in current_user.projects:
+                    current_user.projects.remove(old_project)
+                # Remove the old project from the session and delete it from the database
+                db.session.delete(old_project)
+                # Update the relationships with the new project
+
+                current_user.projects.append(new_project)
+                # Add the new project to the session and commit the changes
+                db.session.add(new_project)
+                db.session.commit()
+                return redirect(url_for('views.home'))
+
     return render_template("home.html",user=current_user)
 
 @views.route('/delete-note', methods=['POST'])
