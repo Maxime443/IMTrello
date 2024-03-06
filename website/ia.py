6bb273d51b5626bfe4c6cb215d02b3b3a6f84030
Maxime Azzi <maxime.azzi@imt-atlantique.net>
2024-03-06 09:51:18 +0100

    ajout estimprojet et avancement

--- website/ia.py
+++ website/ia.py
@@ -1,6 +1,6 @@
 from gpt4all import GPT4All
 
-def time_estim(experiences,desc_tache):
+def estim_tache(experiences,desc_tache):
     #experiences : liste des experiences des developpeurs du projet
     #desc_taches : texte qui décrit la tache à effectuer (sans apostrophe)
 
@@ -9,6 +9,7 @@ def time_estim(experiences,desc_tache):
     prompt1 = f"Je vais te présenter une tâche informatique qui est réalisée par {nombre_developpeurs} développeurs informatiques qui travaillent en collaboration pour réaliser la tâche le plus rapidement possible dont voici les expériences en codage informatique : {', '.join(experiences)}. Tu dois me renvoyer le nombre d heures moyen que tu penses qu il va mettre pour réaliser cette tache. S il te manque du contexte tu peux prendre des initiatives et chercher dans ta base de données, le principal est que tu me renvoies uniquement un nombre. Je veux que ta réponse soit sous la forme d un seul nombre. La tâche est : {desc_tache} "
     prompt2 = 'Renvoie moi uniquement un nombre'
     prompt3 = 'Je veux que ta réponse soit un nombre. Prend toutes les initiatives necessaires. Ne renvoie pas de texte.'
+    
     model = GPT4All(model_name='nous-hermes-llama2-13b.Q4_0.gguf')
     with model.chat_session():
         response1 = model.generate(prompt=prompt1, temp=0)
@@ -17,4 +18,26 @@ def time_estim(experiences,desc_tache):
         last_response = model.current_chat_session[-1]['content']
     return last_response
 
-print(time_estim(['confirmé'],'Réalisation d un algorithme Dijstra simple en Python qui doit permettre de trouver le plus court chemin dans un graphe à 20 noeuds'))
+print(estim_tache(['confirmé'],'Réalisation d un algorithme Dijstra simple en Python qui doit permettre de trouver le plus court chemin dans un graphe à 20 noeuds'))
+
+def estim_projet(taches_projet):
+    #taches_projet : listes des taches que le projet contient 
+    #un élément de la liste est sous la forme [experiences,desc_tache,statut]
+
+    temps=0
+    for t in taches_projet:
+        temps+=estim_tache(t[0],t[1])
+    return temps
+
+
+def avancement_projet(taches_projet):
+    #taches_projet : listes des taches que le projet contient 
+    #un élément de la liste est sous la forme [experiences,desc_tache,statut]
+
+    temps_estime=estim_projet(taches_projet)
+    en_cours=0
+    for t in taches_projet:
+        if t[2]=='completed':
+            en_cours+=estim_tache(t[0],t[1])
+    return en_cours/temps_estime
+
