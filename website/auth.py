ea0eae185afe27fdb7f08d92628f7fdf45b558dd
Kilian Lavenan <kilian.lavenan@imt-atlantique.net>
2024-03-25 10:24:54 +0100

    m

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@ import os
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 
-import app
+
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db  ##means from __init__.py import db
